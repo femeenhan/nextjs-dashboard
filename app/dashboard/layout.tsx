1cdab585c4afc4a6df582ff75ec4b13bea87d9d6
femeen <femeen@naver.com>
2024-11-06 17:10:09 +0900

    수정

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,23 +1,12 @@
-'use client';
-
 import SideNav from '@/app/ui/dashboard/sidenav';
 
-export default function Layout({
-  children,
-  params,
-}: {
-  children: React.ReactNode;
-  params: {
-    tag: string;
-    item: string;
-  };
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen sm:flex-col">
-      <div className="w-[256px] sm:w-full">
+    <div className="flex h-screen pl-[256px] sm:flex-col sm:p-0">
+      <div className="fixed w-[256px] left-0 top-0 h-screen sm:w-full sm:static sm:h-auto">
         <SideNav />
       </div>
-      <div className="p-[48px] flex-grow overflow-y-auto sm:p-6">
+      <div className="p-[48px] flex-grow sm:p-6 overflow-auto sm:overflow-hidden">
         {children}
       </div>
     </div>
