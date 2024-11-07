4fac1126a1acafd827e0af42eab637f15bf8b8e5
femeen <femeen@naver.com>
2024-11-07 17:55:28 +0900

    삭제하시겠습니까?메세지

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { deleteInvoice } from '@/app/lib/action';
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
@@ -28,8 +30,17 @@ export function UpdateInvoice({ id }: { id: string }) {
 export function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
 
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (confirm('삭제하시겠습니까?')) {
+      const result = await deleteInvoiceWithId();
+      console.log(result);
+    }
+  }
+
   return (
-    <form action={deleteInvoiceWithId}>
+    <form action={deleteInvoiceWithId} onSubmit={handleSubmit}>
       <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
